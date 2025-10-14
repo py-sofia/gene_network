87f37354e1458ffbf6f088d69796504375c61a4f
py-sofia <sofia.surace.a@gmail.com>
2025-10-14 15:42:38 +0200

    implemented enrichGO() and compareCluster()

--- enrichment.R
+++ enrichment.R
@@ -2,6 +2,8 @@
 
 library(clusterProfiler)
 library(org.Hs.eg.db)  # assuming human genes
+library(enrichplot)
+
 
 
 modulesMergedEntrez <- lapply(modulesMerged, function(gene_set) {
@@ -13,11 +15,51 @@ modulesMergedEntrez <- lapply(modulesMerged, function(gene_set) {
 })
 
 
-enrichment_results <- lapply(modulesMergedEntrez, function(gene_set) {
-  enrichGO(gene=modulesMergedEntrez$gene_set,
-           OrgDb=org.Hs.eg.db,
-           ont="BP",
-           pAdjustMethod="BH",
-           readable=T,
-           pvalueCutoff=0.05)
+
+universe <- bitr(geneSymbols,
+                 fromType="SYMBOL",
+                 toType="ENTREZID",
+                 OrgDb = org.Hs.eg.db)
+
+enrichmentResults_ontALL <- lapply(modulesMergedEntrez, function(gene_set) {
+  enrichGO(
+    gene = gene_set,
+    OrgDb = org.Hs.eg.db,
+    keyType = "ENTREZID",
+    ont = "ALL",
+    pAdjustMethod = "BH",
+    readable = TRUE,
+    pvalueCutoff = 0.05,
+    qvalueCutoff = 1,
+    universe = universe
+  )
 })
+
+
+
+lapply(names(enrichmentResults_ontALL), function(module_name) {
+  enrich_result <- enrichmentResults_ontALL[[module_name]]
+  
+  if (nrow(as.data.frame(enrich_result)) > 0) {  # only plot if non-empty
+    print(barplot(enrich_result, showCategory = 20, title = module_name))
+    Sys.sleep(5)
+  } else {
+    message(paste("No enrichment results for module:", module_name))
+  }
+})
+
+
+
+compareClusterResult_ontALL <- compareCluster(
+  geneClusters=modulesMergedEntrez,
+  fun="enrichGO",
+  OrgDb=org.Hs.eg.db,
+  keyType="ENTREZID",
+  ont="ALL",
+  pAdjustMethod="BH", # p-values adjusted by cluster
+  readable=T,
+  pvalueCutoff=0.05,
+  qvalueCutoff=1, # do not filter by q-value
+  universe=universe) 
+
+barplot(compareClusterResult_ontALL, showCategory=20)
